5def109208b1ae8fafc48cf643b1dff2392f2fa3
Owen Smith <owen@owensmith.uk>
2024-12-12 01:06:33 +0000

    (wip) etc: Read ADC values

--- src/etc.cc
+++ src/etc.cc
@@ -4,6 +4,7 @@
 #include <hal.hh>
 #include <stm32f103xb.h>
 
+#include <array>
 #include <atomic>
 #include <cstdint>
 #include <utility>
@@ -117,16 +118,72 @@ int main() {
     TIM2->ARR = 15999;
     TIM3->ARR = 7999;
 
+    TIM2->ARR = 999;
+
     // Enable timers and IRQs.
     TIM2->CR1 |= TIM_CR1_CEN;
     TIM3->CR1 |= TIM_CR1_CEN;
     hal::enable_irq(TIM2_IRQn, 4);
     hal::enable_irq(TIM3_IRQn, 3);
 
+    // Enable and calibrate ADC.
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN | RCC_APB2ENR_IOPAEN;
+    RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+
+    // PA5 - ADC1_IN5, PA6 - ADC1_IN6
+    hal::configure_gpio(GPIOA, 5, hal::GpioInputMode::Analog);
+    hal::configure_gpio(GPIOA, 6, hal::GpioInputMode::Analog);
+    ADC1->SQR1 |= ADC_SQR1_L_0;
+    ADC1->SQR3 = (6u << ADC_SQR3_SQ2_Pos) | (5u << ADC_SQR3_SQ1_Pos);
+    ADC1->SMPR2 |= (0b010u << ADC_SMPR2_SMP6_Pos) | (0b010u << ADC_SMPR2_SMP5_Pos);
+
+    ADC1->CR2 |= ADC_CR2_ADON;
+    for (std::uint32_t i = 0; i < 1000; i++) {
+        asm volatile("" ::: "memory");
+    }
+
+    // Perform calibration.
+    ADC1->CR2 |= ADC_CR2_RSTCAL;
+    hal::wait_equal(ADC1->CR2, ADC_CR2_RSTCAL, 0u);
+    ADC1->CR2 |= ADC_CR2_CAL;
+    hal::wait_equal(ADC1->CR2, ADC_CR2_CAL, 0u);
+
+    std::array<std::uint16_t, 12> adc_buffer{};
+    DMA1_Channel1->CPAR = reinterpret_cast<std::uint32_t>(&ADC1->DR);
+    DMA1_Channel1->CMAR = reinterpret_cast<std::uint32_t>(adc_buffer.data());
+    DMA1_Channel1->CNDTR = adc_buffer.size();
+    DMA1_Channel1->CCR = DMA_CCR_PL_1 | DMA_CCR_MSIZE_0 | DMA_CCR_PSIZE_0 | DMA_CCR_MINC | DMA_CCR_CIRC | DMA_CCR_EN;
+
+    ADC1->CR1 |= ADC_CR1_SCAN;
+    ADC1->CR2 |= ADC_CR2_DMA | ADC_CR2_CONT;
+    ADC1->CR2 |= ADC_CR2_ADON;
+
     // Main control loop.
     while (true) {
         if (std::exchange(s_should_send_status, false)) {
             // TODO: Send status report.
+            // const auto adc_value = adc_buffer[0];
+            // std::uint32_t throttle = (adc_value - 1300) * 100;
+            // throttle /= (3000 - 1300);
+            // if (throttle > 100) {
+            //     throttle = 0;
+            // }
+
+            // hal::swd_printf("throttle: %u (raw: [%u, %u])\n", throttle, adc_value, adc_buffer[1]);
+
+            std::uint32_t left_avg = 0;
+            std::uint32_t right_avg = 0;
+            for (std::uint32_t i = 0; i < adc_buffer.size(); i += 2) {
+                left_avg += adc_buffer[i];
+            }
+            for (std::uint32_t i = 1; i < adc_buffer.size(); i += 2) {
+                right_avg += adc_buffer[i];
+            }
+            left_avg /= adc_buffer.size() / 2;
+            right_avg /= adc_buffer.size() / 2;
+            std::uint32_t left = (left_avg + 2) / 5 * 5;
+            std::uint32_t right = (right_avg + 2) / 5 * 5;
+            hal::swd_printf("left: %u, right: %u\n", left, right);
         }
 
         if (std::exchange(s_should_update_dti, false)) {
@@ -134,6 +191,6 @@ int main() {
         }
 
         // Sleep until next interrupt.
-        __WFI();
+        // __WFI();
     }
 }
